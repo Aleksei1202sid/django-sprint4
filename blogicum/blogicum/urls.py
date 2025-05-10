3ab8893fbf41c5b4e061e3c709b2c0fe6f92f9c3
Aleksei Sidorenko <Sidorenko1202@yandex.ru>
2025-05-10 20:27:31 +0300

    New_commit_4

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic.edit import CreateView
-# from users.forms import CustomUserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 
 app_name = 'blogicum'
 
@@ -16,7 +16,7 @@ urlpatterns = [
         'auth/registration/',
         CreateView.as_view(
             template_name='registration/registration_form.html',
-            # form_class=CustomUserCreationForm,
+            form_class=UserCreationForm,
             success_url=reverse_lazy('blog:index'),
         ),
         name='registration',

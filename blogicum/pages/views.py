9dc7cd403accdba6f2ecda60dd79231e1c44c1ad
Aleksei Sidorenko <Sidorenko1202@yandex.ru>
2025-05-13 00:05:10 +0300

    new_commit_4

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+
+def about(request):
+    template = 'pages/about.html'
+    return render(request, template)
 
 
-class About(TemplateView):
-    template_name = 'pages/about.html'
-
-
-class Rules(TemplateView):
-    template_name = 'pages/rules.html'
+def rules(request):
+    template = 'pages/rules.html'
+    return render(request, template)
 
 
 def page_not_found(request, exception):
@@ -18,5 +18,5 @@ def csrf_failure(request, reason=''):
     return render(request, 'pages/403csrf.html', status=403)
 
 
-def server_error(request):
+def internal_server_error(request):
     return render(request, 'pages/500.html', status=500)
--- blogicum/users/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-
-
-class MyUser(AbstractUser):
-    pass
-    # bio = models.TextField('Биография', blank=True)
